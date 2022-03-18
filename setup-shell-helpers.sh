9af3adbd2483131deec72110b7ce6b1592f66c04
Leo Chen <lchen@rtr.ai>
2022-03-18 13:02:00 -0400

    fix shell helper

--- setup-shell-helpers.sh
+++ setup-shell-helpers.sh
@@ -3,6 +3,13 @@
 # SHELL_CONFIG_DIR this need to point to the repo folder this file is in. The scirpt rely on this 
 # to find relative file location
 
-[ -z ${SHELL_CONFIG_DIR+x} ] && SHELL_CONFIG_DIR="$(dirname $(realpath $0))"
 
-. "${SHELL_CONFIG_DIR}/shell-helpers.d/git-func.sh"
+
+if [ -z ${SHELL_CONFIG_DIR+x} ] ; then 
+    SHELL_HELPER_DIR="$(dirname $(realpath $0))"
+else 
+    SHELL_HELPER_DIR=${SHELL_CONFIG_DIR}/shell-helpers.d
+fi 
+
+. "${SHELL_HELPER_DIR}/git-func.sh"
+. "${SHELL_HELPER_DIR}/systemd-func.sh"
